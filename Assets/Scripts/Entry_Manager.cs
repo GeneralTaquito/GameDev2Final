8470746109bdbdc1c1781f7bfd9f59c771dc2d35
Charles <charlescastro521@gmail.com>
2026-05-08 17:29:23 -0400

    da
    da

--- Assets/Scripts/Entry_Manager.cs
+++ Assets/Scripts/Entry_Manager.cs
@@ -1,16 +1,62 @@
+using System.Collections;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class Entry_Manager : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    public Collider BookSlot;
+    public GameObject Book1;
+    public enum Rooms
     {
-        
+        None = 0,
+        Room1 = 1,
+        Room2 = 2,
+        Room3 = 3,
     }
+    public Rooms roomVal;
 
-    // Update is called once per frame
+    [Header("DoorVars")]
+    public GameObject myDoor;
+    public Material doorMat;
+
     void Update()
     {
-        
+        switch (roomVal)
+        {
+            case Rooms.None:
+                StartCoroutine(FadeDoor(60f));
+                Debug.Log("defaultstate");
+                break;
+
+            case Rooms.Room1:
+                
+            break;
+
+        }
     }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Book1"))
+        {
+            
+            Debug.Log("Bookplaced");
+        }
+    }
+
+    IEnumerator FadeDoor(float speed)
+    {
+        while(doorMat.color.a > 0)
+        {
+            Color Color = doorMat.color;
+            Color.a -= 1f;
+            yield return new WaitForSeconds(1 / speed);
+            Debug.Log("reachedhere");
+        }
+        if (doorMat.color.a == 0)
+        {
+            myDoor.SetActive(false);
+        }
+    }
+
 }
